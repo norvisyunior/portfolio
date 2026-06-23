1e8e31d77e8ec0399b170132f700483483d1fe53
Norvis Yunior <norviscabrera7@gmail.com>
2026-06-23 17:24:50 -0400

    Elimina enlace de LinkedIn incorrecto de los datos personales

--- src/data/personal.js
+++ src/data/personal.js
@@ -4,7 +4,6 @@ export const personalInfo = {
   location: 'Matanzas, Cuba',
   email: 'norviscabrera7@gmail.com',
   phone: '+53 54845283',
-  linkedin: 'https://linkedin.com/in/midudev',
   github: 'https://github.com/norvisyunior',
   bio: 'Ingeniero en Ciencias Informáticas graduado en diciembre de 2024 por la Universidad de Matanzas. Experiencia en el desarrollo e implementación de aplicaciones web y de escritorio utilizadas en entornos reales, incluyendo sistemas de gestión empresarial, control de inventarios, comercio electrónico y administración de recursos.',
   shortBio: 'Apasionado por crear soluciones web y de escritorio que resuelven problemas reales.',
